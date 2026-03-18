e40b7a4414ac3efc2d8cb9f4d73fadfdc2c53e6d
CrSjimo <crs_16423@outlook.com>
2026-03-18 12:03:47 +0800

    Support prettified list-device output

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26.1
 
 require (
 	github.com/gin-gonic/gin v1.12.0
+	github.com/jedib0t/go-pretty/v6 v6.7.8
 	github.com/spf13/cobra v1.10.2
 	github.com/spf13/viper v1.21.0
 )
@@ -12,6 +13,7 @@ require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
+	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
@@ -27,6 +29,7 @@ require (
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.21 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
--- internal/utils/list_devices.go
+++ internal/utils/list_devices.go
@@ -21,7 +21,11 @@ package utils
 import (
 	"diffscope-synthesis-platform/native"
 	"encoding/json"
+	"fmt"
 	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 type jsonDeviceInfo struct {
@@ -67,6 +71,24 @@ func toJSONDeviceInfo(device native.DeviceInfo) jsonDeviceInfo {
 	}
 }
 
+func formatMemorySize(memory uint64) string {
+	const (
+		KB = 1024
+		MB = 1024 * KB
+		GB = 1024 * MB
+	)
+	switch {
+	case memory >= GB:
+		return fmt.Sprintf("%.2f GiB", float64(memory)/float64(GB))
+	case memory >= MB:
+		return fmt.Sprintf("%.2f MiB", float64(memory)/float64(MB))
+	case memory >= KB:
+		return fmt.Sprintf("%.2f KiB", float64(memory)/float64(KB))
+	default:
+		return fmt.Sprintf("%d B", memory)
+	}
+}
+
 func ListDevices(shouldPrintAsJson bool) {
 	executionProviders := native.ExecutionProviderInfoGetExecutionProviders()
 	defaultDevice := native.ExecutionProviderInfoGetDefaultDevice()
@@ -96,6 +118,68 @@ func ListDevices(shouldPrintAsJson bool) {
 			panic(err)
 		}
 	} else {
+		twStyle := table.StyleRounded
+		twStyle.Options.SeparateRows = true
+		twStyle.Format.Header = text.FormatDefault
+		for i := int64(0); i < executionProviders.Size(); i++ {
+			provider := executionProviders.Get(int(i))
+			providerType := provider.Type()
+			tw := table.NewWriter()
+			tw.SetStyle(twStyle)
+			providerName := executionProviderTypeToString(providerType)
 
+			if providerType == native.ExecutionProviderType_CPU || providerType == native.ExecutionProviderType_CoreML {
+				tw.AppendRow(
+					table.Row{providerName},
+				)
+				fmt.Println(tw.Render())
+				continue
+			} else {
+				tw.AppendHeader(
+					table.Row{providerName, providerName, providerName, providerName},
+					table.RowConfig{AutoMerge: true},
+				)
+			}
+
+			tw.AppendHeader(table.Row{"Index", "Description", "ID", "Memory"})
+
+			devices := provider.Devices()
+			for j := int64(0); j < devices.Size(); j++ {
+				device := devices.Get(int(j))
+				tw.AppendRow(table.Row{
+					device.Index(),
+					device.Description(),
+					device.Id(),
+					formatMemorySize(device.Memory()),
+				})
+			}
+
+			fmt.Println(tw.Render())
+		}
+		fmt.Println()
+		fmt.Printf("Default device:\n")
+		tw := table.NewWriter()
+		tw.SetStyle(twStyle)
+		providerType := defaultDevice.Type()
+		providerName := executionProviderTypeToString(providerType)
+		if providerType == native.ExecutionProviderType_CPU || providerType == native.ExecutionProviderType_CoreML {
+			tw.AppendRow(
+				table.Row{providerName},
+			)
+			fmt.Println(tw.Render())
+		} else {
+			tw.AppendHeader(
+				table.Row{providerName, providerName, providerName, providerName},
+				table.RowConfig{AutoMerge: true},
+			)
+			tw.AppendHeader(table.Row{"Index", "Description", "ID", "Memory"})
+			tw.AppendRow(table.Row{
+				defaultDevice.Index(),
+				defaultDevice.Description(),
+				defaultDevice.Id(),
+				formatMemorySize(defaultDevice.Memory()),
+			})
+			fmt.Println(tw.Render())
+		}
 	}
 }
